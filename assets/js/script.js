5076157fd4f2bbc5e675ce7b9c7ff8370d7aba06
tarcisttobias <tarcistt@live.com>
2023-07-07 08:43:07 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@
  * element toggle function
  */
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elemToggleFunc = function (elem) {
+    elem
+        .classList
+        .toggle("active");
+}
 
 /**
  * header sticky & go to top
@@ -17,20 +19,30 @@ const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
 
-  if (window.scrollY >= 10) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
+    if (window.scrollY >= 10) {
+        header
+            .classList
+            .add("active");
+        goTopBtn
+            .classList
+            .add("active");
+    } else {
+        header
+            .classList
+            .remove("active");
+        goTopBtn
+            .classList
+            .remove("active");
+    }
 
 });
 
-
-
 /**
  * navbar toggle
+*/
+
+/*
+Old
 
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
@@ -47,27 +59,21 @@ navToggleBtn.addEventListener("click", function () {
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
 
-
-
 navToggleBtn.addEventListener("click", function () {
-  elemToggleFunc(navToggleBtn);
-  elemToggleFunc(navbar);
-  elemToggleFunc(document.body);
+    elemToggleFunc(navToggleBtn);
+    elemToggleFunc(navbar);
+    elemToggleFunc(document.body);
 });
 
 const navLinks = document.querySelectorAll(".navbar-link");
 
 navLinks.forEach(function (link) {
-  link.addEventListener("click", function () {
-    elemToggleFunc(navToggleBtn);
-    elemToggleFunc(navbar);
-    elemToggleFunc(document.body);
-  });
+    link.addEventListener("click", function () {
+        elemToggleFunc(navToggleBtn);
+        elemToggleFunc(navbar);
+        elemToggleFunc(document.body);
+    });
 });
-
-  
-
-
 
 /**
  * skills toggle
@@ -78,16 +84,16 @@ const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
 const skillsBox = document.querySelector("[data-skills-box]");
 
 for (let i = 0; i < toggleBtns.length; i++) {
-  toggleBtns[i].addEventListener("click", function () {
+    toggleBtns[i].addEventListener("click", function () {
 
-    elemToggleFunc(toggleBtnBox);
-    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
-    elemToggleFunc(skillsBox);
+        elemToggleFunc(toggleBtnBox);
+        for (let i = 0; i < toggleBtns.length; i++) {
+            elemToggleFunc(toggleBtns[i]);
+        }
+        elemToggleFunc(skillsBox);
 
-  });
+    });
 }
-
-
 
 /**
  * dark & light theme toggle
@@ -97,19 +103,31 @@ const themeToggleBtn = document.querySelector("[data-theme-btn]");
 
 themeToggleBtn.addEventListener("click", function () {
 
-  elemToggleFunc(themeToggleBtn);
+    elemToggleFunc(themeToggleBtn);
 
-  if (themeToggleBtn.classList.contains("active")) {
-    document.body.classList.remove("dark_theme");
-    document.body.classList.add("light_theme");
+    if (themeToggleBtn.classList.contains("active")) {
+        document
+            .body
+            .classList
+            .remove("dark_theme");
+        document
+            .body
+            .classList
+            .add("light_theme");
 
-    localStorage.setItem("theme", "light_theme");
-  } else {
-    document.body.classList.add("dark_theme");
-    document.body.classList.remove("light_theme");
+        localStorage.setItem("theme", "light_theme");
+    } else {
+        document
+            .body
+            .classList
+            .add("dark_theme");
+        document
+            .body
+            .classList
+            .remove("light_theme");
 
-    localStorage.setItem("theme", "dark_theme");
-  }
+        localStorage.setItem("theme", "dark_theme");
+    }
 
 });
 
@@ -118,38 +136,57 @@ themeToggleBtn.addEventListener("click", function () {
  */
 
 if (localStorage.getItem("theme") === "light_theme") {
-  themeToggleBtn.classList.add("active");
-  document.body.classList.remove("dark_theme");
-  document.body.classList.add("light_theme");
+    themeToggleBtn
+        .classList
+        .add("active");
+    document
+        .body
+        .classList
+        .remove("dark_theme");
+    document
+        .body
+        .classList
+        .add("light_theme");
 } else {
-  themeToggleBtn.classList.remove("active");
-  document.body.classList.remove("light_theme");
-  document.body.classList.add("dark_theme");
+    themeToggleBtn
+        .classList
+        .remove("active");
+    document
+        .body
+        .classList
+        .remove("light_theme");
+    document
+        .body
+        .classList
+        .add("dark_theme");
 }
-
 
 /**
  * Switch language
  */
 
 function changeLang() {
-  var ddl = document.getElementById("lang");
-  var selectedValue = ddl.options[ddl.selectedIndex].value;
-  document.documentElement.setAttribute("lang", selectedValue);
-  var lang = selectedValue;
-  var elements = document.querySelectorAll("[lang]");
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].getAttribute("lang") === lang) {
-      elements[i].style.display = "";
-    } else {
-      elements[i].style.display = "none";
+    var ddl = document.getElementById("lang");
+    var selectedValue = ddl
+        .options[ddl.selectedIndex]
+        .value;
+    document
+        .documentElement
+        .setAttribute("lang", selectedValue);
+    var lang = selectedValue;
+    var elements = document.querySelectorAll("[lang]");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].getAttribute("lang") === lang) {
+            elements[i].style.display = "";
+        } else {
+            elements[i].style.display = "none";
+        }
     }
-  }
 }
 
 var select = document.getElementById("lang");
-select.addEventListener("change", function() {
-  changeLang();
+select.addEventListener("change", function () {
+    changeLang();
 });
 
 changeLang();
